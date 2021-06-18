ff6319dbcb88829d4e7666b330d80f9702304781
akobeleva <a.kobeleva@g.nsu.ru>
2021-06-18 18:06:18 +0700

    add autoloader

---
+++ core/Router.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace core;
+
+class Router
+{
+    public function __construct()
+    {
+
+    }
+
+    public function add()
+    {
+    }
+
+    public function match()
+    {
+    }
+
+    public function run()
+    {
+        echo 'start router';
+    }
+
+}
--- index.php
+++ index.php
@@ -1,2 +1,42 @@
 <?php
+
+use core\Router;
+
 echo 'hello';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = '';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . DIRECTORY_SEPARATOR;
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+session_start();
+
+$router = new Router();
+$router->run();

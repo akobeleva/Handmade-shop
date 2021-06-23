48c0770669e33a7901bd2f28daf03be8d97bb40d
akobeleva <a.kobeleva@g.nsu.ru>
2021-06-23 18:21:13 +0700

    add header to default layout

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -4,17 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="public/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/css/main.css" rel="stylesheet">
 
-    <title>DEFAULT</title>
+    <title>Handmade shop</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
-<?=$content?>
+<header>
+    <a href="/" class="logo"><img src="public/img/logo.jpg" alt=""></a>
+    <div class="buttons">
+        <a href="/" class="btn_bag"><img src="public/img/shopping-bag.png" alt=""></a>
+        <a href="/" class="btn_user"><img src="public/img/avatar.png" alt=""></a>
+        <a href="/" class="btn_signin">Sign In</a>
+    </div>
+</header>
+<?= $content ?>
 Дефолтный шаблон
-<script
-        src="public/bootstrap/dist/js/bootstrap.bundle.min.js">
-</script>
 </body>
 </html>

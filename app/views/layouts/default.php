f677801bd1d38332c12b7ef865e7b2a150321843
akobeleva <a.kobeleva@g.nsu.ru>
2021-06-24 18:27:29 +0700

    change file's name in default.php; translate text in 404.html; fix body max-width in main.css

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -4,20 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="public/css/main.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
 
     <title>Handmade shop</title>
 </head>
 <body>
 <header>
-    <a href="/" class="logo"><img src="public/img/logo.jpg" alt=""></a>
+    <a href="/" class="logo"><img src="/img/logo.jpg" alt=""></a>
     <div class="buttons">
-        <a href="/" class="btn_bag"><img src="public/img/shopping-bag.png" alt=""></a>
-        <a href="/" class="btn_user"><img src="public/img/avatar.png" alt=""></a>
+        <a href="/" class="btn_bag"><img src="/img/shopping-bag.png" alt=""></a>
+        <a href="/" class="btn_user"><img src="/img/avatar.png" alt=""></a>
         <a href="/" class="btn_signin">Sign In</a>
     </div>
 </header>
 <?= $content ?>
-Дефолтный шаблон
 </body>
 </html>

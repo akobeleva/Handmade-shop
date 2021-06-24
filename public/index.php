ff9e8d530ddb5ef60f503274a104dcd7629419e7
akobeleva <a.kobeleva@g.nsu.ru>
2021-06-24 18:25:37 +0700

    change paths for files

--- public/index.php
+++ public/index.php
@@ -4,17 +4,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 use core\Router;
 
-require 'autoload.php';
-require '../libs/functions.php';
+require_once 'autoload.php';
+require_once '../libs/functions.php';
 session_start();
 
-$query = rtrim($_SERVER['REQUEST_URI'], '/');
 $router = new Router();
-
-$router->addRoute('^$', ['controller' => 'Main', 'action' => "index"]);
-$router->addRoute('(?P<controller>[a-z-]+)/?(?P<>[a-z-]+)?');
-
-$router->dispatch($query);
+$router->dispatch();

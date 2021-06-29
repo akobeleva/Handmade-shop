500d5b01b40426a0f80e9f680a1edda969dc68d4
akobeleva <a.kobeleva@g.nsu.ru>
2021-06-29 15:03:47 +0700

    add class and config for db connection

---
+++ database/DB.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class DB
+{
+    protected $pdo;
+    protected static $instance;
+
+    protected function __construct()
+    {
+        $db = require_once 'config/database.php';
+        $this->pdo = new PDO($db['dsn'], $db['user'], $db['pass']);
+    }
+
+    public static function getInstance(): DB
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function execute(string $sql): bool
+    {
+        $statement = $this->pdo->prepare($sql);
+        return $statement->execute();
+    }
+
+    public function query($sql): array
+    {
+        $statement = $this->pdo->prepare($sql);
+        $result = $statement->execute();
+        if ($result !== false) {
+            return $statement->fetchAll();
+        }
+        return [];
+    }
+}

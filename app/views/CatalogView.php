119a0cd067917fb2d2ef0a02e91b13718573e07e
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-06 17:24:23 +0700

    CatalogView.php, main.css: add category's cards

--- app/views/CatalogView.php
+++ app/views/CatalogView.php
@@ -1,1 +1,22 @@
-<h1>Каталог</h1>
+<div class="catalog">
+    <h1>Каталог</h1>
+    <div class="row wow fadeIn">
+        <?php
+        if (isset($data)) {
+            foreach ($data as $item): ?>
+                <div class="col-lg-3 mb-4">
+                    <div class="card">
+                        <a href="/"><img class="card-img-top" src="/img/avatar.png" alt=""></a>
+                        <div class="card-body text-center">
+                            <a href="/" class="grey-text">
+                                <h5><?php
+                                    echo $item['name'] ?></h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            endforeach;
+        } ?>
+    </div>
+</div>

f821fe2cffb6f7bca1f5faf469648b74687ed125
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-08 14:19:27 +0700

    View.php: define path, add function

--- core/View.php
+++ core/View.php
@@ -2,19 +2,20 @@
 
 namespace core;
 
+define('VIEW_ROOT', '../app/views/');
+
 class View
 {
-    public function renderPage(string $viewName, $data = null, string $templateName = 'MainTemplate.php')
+    public function renderPage(string $viewName, $data = null, string $templateName = 'main_tpl.php')
     {
         if (is_array($data)) {
             extract($data);
         }
-        $file_view = '../app/views/' . $viewName;
+        $file_view = VIEW_ROOT . $viewName;
         ob_start();
         require_once $file_view;
         $content = ob_get_clean();
-
-        $file_layout = '../app/views/layouts/' . $templateName;
+        $file_layout = VIEW_ROOT . 'templates/' . $templateName;
         require_once $file_layout;
     }
 
@@ -23,6 +24,13 @@ class View
         if (is_array($data)) {
             extract($data);
         }
-        require_once '../app/views/' . $componentViewName;
+        require_once VIEW_ROOT . $componentViewName;
+    }
+
+    public function render($title, $vars = []){
+        ob_start();
+        require_once VIEW_ROOT . 'templates/content_tpl.php';
+        $content = ob_get_clean();
+        require_once VIEW_ROOT . 'templates/main_tpl.php';
     }
 }

faa21ae1b64da93c197b9dd361802853d1cf9fd9
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-09 11:53:57 +0700

    CatalogController.php: add _GET params handler

--- app/controllers/CatalogController.php
+++ app/controllers/CatalogController.php
@@ -16,7 +16,27 @@ class CatalogController extends Controller
 
     public function indexAction()
     {
-        $data = $this->model->getAllRows();
-        $this->view->renderPage('CatalogView.php', $data);
+        if (!$_GET){
+            $data = $this->model->getAllRows();
+            $this->view->renderPage('CatalogView.php', $data);
+        }
+        else $this->categoryAction($_GET['category']);
+    }
+
+    private function categoryAction($categoryId){
+        $subCatController = new SubcategoryController();
+        $subcategories = $subCatController->subcategoryAction($categoryId);
+        $leftMenu = $this->view->renderTemplate('left_menu_tpl.php', ['menuItems'=>$subcategories]);
+        $content = $this->view->renderTemplate(
+            'simple_page_tpl.php',
+            [
+                'title' => 'Каталог',
+                'leftMenu'  => $leftMenu
+            ]
+        );
+        echo $this->view->renderTemplate(
+            'main_tpl.php',
+            ['content' => $content]
+        );
     }
 }

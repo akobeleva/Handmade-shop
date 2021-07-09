29c041039ecd0f5f71223a4a475bb6420b5b3924
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-09 13:24:05 +0700

    left_menu_tpl.php:rename items

--- app/views/templates/left_menu_tpl.php
+++ app/views/templates/left_menu_tpl.php
@@ -1,11 +1,11 @@
-<div class="container content">
+<div>
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <?php
-            if (isset($menuItems)): ?>
+            if (isset($leftMenuItems)): ?>
                 <div class="list-group">
                     <?php
-                    foreach ($menuItems as $item): ?>
+                    foreach ($leftMenuItems as $item): ?>
                         <a href="/catalog/?category=<?php
                         echo $item['category_id'] ?>&subcategory=<?php
                         echo $item['id'] ?>" class="list-group-item"><?php

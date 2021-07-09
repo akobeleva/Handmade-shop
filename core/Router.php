fc24f7154aaea527d0a4bfb4adb25e55ab05cb8d
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-09 17:55:03 +0700

    Router.php: add route

--- core/Router.php
+++ core/Router.php
@@ -17,6 +17,7 @@ class Router
         $this->addRoute('/contacts', SimplePageController::class, 'contactsAction');
         $this->addRoute('/catalog', CatalogController::class, 'indexAction');
         $this->addRoute('/catalog/category', CatalogController::class, 'categoryAction');
+        $this->addRoute('/catalog/category/subcategory', CatalogController::class, 'subcategoryAction');
     }
 
     public static function addRoute(

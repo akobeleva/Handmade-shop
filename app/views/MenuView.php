0d0655f3ea8c6ea4a00f5f52aa7cdd91e7738262
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-09 16:10:47 +0700

    MenuView.php: rename menuItems and add link to item

--- app/views/MenuView.php
+++ app/views/MenuView.php
@@ -1,10 +1,10 @@
 <ul class="menu_list">
     <li><a href="/catalog">Каталог</a></li>
     <?php
-    if (isset($data)) {
-        foreach ($data as $item): ?>
-            <li><a href="/"><?php
-                    echo $item['name'] ?></a></li>
+    if (isset($menuItems)) {
+        foreach ($menuItems as $item): ?>
+            <li><a href="/catalog/?category=<?php echo $item['id'];?>"><?php
+                    echo $item['name'];?></a></li>
         <?php
         endforeach;
     } ?>

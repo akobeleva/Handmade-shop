84dcfea8cfbbbd7586d7aad8fe8e8875777e3eff
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-13 15:14:33 +0700

    catalog_tpl.php, main.css: add differences between main catalog and category catalog

--- app/views/templates/catalog_tpl.php
+++ app/views/templates/catalog_tpl.php
@@ -1,22 +1,57 @@
 <div class="catalog">
     <div class="row wow fadeIn">
         <?php
-        if (isset($catalogItems)) {
+        if (isset($catalogItems)):?>
+            <?php
             foreach ($catalogItems as $item): ?>
                 <div class="col-lg-4 mb-4">
-                    <div class="card pt-3">
-                        <a href="catalog/category/?id=<?php echo $item['id'] ?>"><img class="card-img-top" src="/img/<?php
-                            echo $item['image_name'] ?>" alt=""></a>
-                        <div class="card-body text-center">
-                            <a href="catalog/category/?id=<?php echo $item['id'] ?>" class="grey-text">
+                    <div class="card p-3">
+                        <a href="catalog/category/?id=<?php
+                        echo $item['id'] ?>">
+                            <img class="card-img-top
+                            <?php
+                            if (isset($item['subcategory_id'])) {
+                                echo 'product_card';
+                            }
+                            ?> " src="/img/<?php
+                            echo $item['image_name'] ?>"
+                                 alt="">
+                        </a>
+                        <div class="card-body">
+                            <a href="catalog/category/?id=<?php
+                            echo $item['id'] ?>" class="grey-text text-center ">
                                 <h5><?php
                                     echo $item['name'] ?></h5>
                             </a>
+                            <div>
+                                <?php
+                                if (isset($item['price'])): ?>
+                                    <span class=" product-price"><?php
+                                        echo $item['price'] . '₽' ?></span>
+                                <?php
+                                endif; ?>
+                                <?php
+                                if (isset($item['price'])): ?>
+                                    <div class="product_buttons text-end">
+                                        <button type="button"
+                                                class="btn btn btn-outline-info btn-circle">
+                                            <img src="/img/heart.png"
+                                                 alt=""></button>
+                                        <button type="button"
+                                                class="btn btn btn-outline-info btn-circle">
+                                            <img src="/img/shopping-bag.png"
+                                                 alt=""></button>
+                                    </div>
+                                <?php
+                                endif; ?>
+                            </div>
                         </div>
                     </div>
                 </div>
             <?php
-            endforeach;
-        } ?>
+            endforeach; ?>
+        <?php
+        endif;
+        ?>
     </div>
 </div>

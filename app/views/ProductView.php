56a2f6fb14fc4e9fe3a522e2329fe0016f01ba6a
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-14 17:10:02 +0700

    ProductView.php: add view

--- app/views/ProductView.php
+++ app/views/ProductView.php
@@ -1,4 +1,49 @@
 <?php
-if (isset($product)){
-    debug($product);
-}
+
+if (isset($product)):
+    ?>
+    <div class="container mt-4 mb-4">
+        <div class="row g-3">
+            <div class="col-lg-8">
+                <div class="border p-5">
+                    <img class="img-fluid mx-auto d-block" src="/img/<?php
+                    echo $product['image_name'] ?>" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="border p-3">
+                    <h4><?php
+                        echo $product['name']; ?></h4>
+                    <span class='price'>Цена: <?php
+                        echo $product['price'] . '₽'; ?></span>
+                    <div class="product_buttons text-end">
+                        <button type="button"
+                                class="btn btn-info btn-floating">
+                            <img src="/img/heart.png"
+                                 alt=""></button>
+                        </button>
+                        <button type="button" class="btn btn-info">В корзину
+                        </button>
+                    </div>
+                </div>
+                <div class="border p-3">
+                    <span class='seller'>Продавец: <?php
+                        echo $product['seller_id']; ?></span>
+                    <div class="text-end">
+                        <button type="button" class="btn btn-outline-dark">
+                            Контакты продавца
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="border p-3">
+                    <h4>Описание товара</h4>
+                    <span class="description"><?php
+                        echo $product['description']; ?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+endif; ?>

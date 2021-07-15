e266b6916f096fc6d60fede8de1849f579b7b1e6
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-15 18:09:45 +0700

    Add SearchController.php

---
+++ app/controllers/SearchController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\SearchModel;
+use core\Controller;
+use core\View;
+
+class SearchController extends Controller
+{
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->model = new SearchModel();
+    }
+
+    public function indexAction($_get)
+    {
+    }
+}

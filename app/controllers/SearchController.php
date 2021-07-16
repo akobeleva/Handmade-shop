2ca3e81ec10ce213abd3147c90d184297db0e663
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-16 17:24:22 +0700

    Delete field view

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -4,13 +4,11 @@ namespace app\controllers;
 
 use app\models\SearchModel;
 use core\Controller;
-use core\View;
 
 class SearchController extends Controller
 {
     public function __construct()
     {
-        $this->view = new View();
         $this->model = new SearchModel();
     }
 
--- app/controllers/SubcategoryController.php
+++ app/controllers/SubcategoryController.php
@@ -4,13 +4,11 @@ namespace app\controllers;
 
 use app\models\SubcategoryModel;
 use core\Controller;
-use core\View;
 
 class SubcategoryController extends Controller
 {
     public function __construct()
     {
-        $this->view = new View();
         $this->model = new SubcategoryModel();
     }
 

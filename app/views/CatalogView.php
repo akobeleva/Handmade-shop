a539c57fc29297655c71e9af3c4d0d817c51fe69
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-16 19:55:59 +0700

    CatalogView.php: add vars for cards

--- app/views/CatalogView.php
+++ app/views/CatalogView.php
@@ -8,6 +8,13 @@ class CatalogView extends View
 {
     public function render($vars = [])
     {
+        foreach ($vars['catalogItems'] as &$item) {
+            $cardView = new CardView();
+            $item['link'] = '/catalog/category';
+            $item['cardBody'] = $cardView->renderBody($item);
+            $item['card'] = $cardView->renderCard($item);
+        }
+        $vars['columnWidth'] = 3;
         $catalog = $this->renderTemplate('catalog_tpl.php', $vars);
         $vars['text'] = $catalog;
         $vars['title'] = 'Каталог';

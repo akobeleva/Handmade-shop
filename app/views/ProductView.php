aeab72169ce683a7b07432807873cb7a6e08b448
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-16 17:32:06 +0700

    Move product_view.php to templates and rename to product_tpl.php

--- app/views/ProductView.php
+++ app/views/ProductView.php
@@ -7,7 +7,7 @@ use core\View;
 class ProductView extends View
 {
     public function render($vars = []){
-        $text = $this->renderTemplate('product_view.php', $vars);
+        $text = $this->renderTemplate('product_tpl.php', $vars);
         $vars['text'] = $text;
         $pageContentView = new PageContentView();
         $pageContentView->render($vars);

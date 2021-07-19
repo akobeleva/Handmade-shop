c9634fb041478c68ca89719dcb1844ebd80575a2
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-19 16:45:11 +0700

    SubcategoryController.php: add function and view

--- app/controllers/SubcategoryController.php
+++ app/controllers/SubcategoryController.php
@@ -3,13 +3,28 @@
 namespace app\controllers;
 
 use app\models\SubcategoryModel;
+use app\views\SidebarPageView;
 use core\Controller;
 
 class SubcategoryController extends Controller
 {
     public function __construct()
     {
+        $this->view = new SidebarPageView();
         $this->model = new SubcategoryModel();
+    }
+
+    public function showSubcategoryPage($_get)
+    {
+        $subcategory = $this->getSubcategoryById($_get['id']);
+        $subcategories = $this->getSubcategoriesByCategoryId($subcategory[0]['category_id']);
+        $vars['leftMenuItems'] = $subcategories;
+        $productController = new ProductController();
+        $title = $subcategory[0]['name'];
+        $products = $productController->getProductsBySubcategoryId($_get['id']);
+        $vars['title'] = $title;
+        $vars['catalogItems'] = $products;
+        $this->view->renderCategoryPage($vars);
     }
 
     public function getSubcategoriesByCategoryId($categoryId): array

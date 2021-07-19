3c3beeddcf92beea1d2fd4887b6164ced98fab7a
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-19 14:14:24 +0700

    PageView.php: rename function renderMainView to printPage, add function

--- app/views/PageView.php
+++ app/views/PageView.php
@@ -10,11 +10,24 @@ class PageView extends View
     {
         $content = $this->renderTemplate('content_tpl.php', $vars);
         $vars['content'] = $content;
-        $this->renderMainView($vars);
+        $this->printPage($vars);
     }
 
-    private function renderMainView($vars = [])
+    private function printPage($vars = [])
     {
         echo $this->renderTemplate('main_tpl.php', $vars);
+    }
+
+    public function renderCatalogPage($vars = [])
+    {
+        if (isset($vars['catalogItems'])) {
+            $catalogView = new CatalogView();
+            $text = $catalogView->renderCatalogView($vars);
+            $vars['title'] = 'Каталог';
+            $vars['text'] = $text;
+            $content = $this->renderTemplate('content_tpl.php', $vars);
+            $vars['content'] = $content;
+            $this->printPage($vars);
+        }
     }
 }

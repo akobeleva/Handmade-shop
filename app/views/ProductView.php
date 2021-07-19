ceb9cfa3091c87ff5f95db639e5824f3a1cea027
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-19 16:06:50 +0700

    PageView.php: add return value

--- app/views/ProductView.php
+++ app/views/ProductView.php
@@ -6,10 +6,8 @@ use core\View;
 
 class ProductView extends View
 {
-    public function render($vars = []){
-        $text = $this->renderTemplate('product_tpl.php', $vars);
-        $vars['text'] = $text;
-        $pageContentView = new PageView();
-        $pageContentView->render($vars);
+    public function renderProductView($vars = [])
+    {
+        return $this->renderTemplate('product_tpl.php', $vars);
     }
 }

2c53cedfdf2a784339a96de3a19659fc75d83cc9
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-19 16:48:36 +0700

    SidebarPageView.php: change link for subcategory page

--- app/views/SidebarPageView.php
+++ app/views/SidebarPageView.php
@@ -13,8 +13,7 @@ class SidebarPageView extends PageView
         $rightContent = $catalogView->renderCatalogView($vars);
         $vars['rightContent'] = $rightContent;
         foreach ($vars['leftMenuItems'] as &$item) {
-            $item['link'] = "/catalog/category/?id=" . $item['category_id']
-                . "&subcategory=" . $item['id'];
+            $item['link'] = "/catalog/subcategory/?id=" . $item['id'];
         }
         $text = $this->renderTemplate('sidebar_tpl.php', $vars);
         $vars['text'] = $text;

97b21f9a6b3eed401984bad34e56b74ba68c5b0e
akobeleva <a.kobeleva@g.nsu.ru>
2021-07-19 16:18:58 +0700

    menu_tpl.php: move link definition to MenuView.php

--- app/views/MenuView.php
+++ app/views/MenuView.php
@@ -8,6 +8,9 @@ class MenuView extends View
 {
     public function renderMenu($vars = [])
     {
+        foreach ($vars['menuItems'] as &$item) {
+            $item['link'] = "/catalog/category/?id=" . $item['id'];
+        }
         echo $this->renderTemplate('menu_tpl.php', $vars);
     }
 }
--- app/views/templates/menu_tpl.php
+++ app/views/templates/menu_tpl.php
@@ -1,11 +1,13 @@
 <ul class="menu_list">
     <li><a href="/catalog">Каталог</a></li>
     <?php
-    if (isset($menuItems)) {
-        foreach ($menuItems as $item): ?>
-            <li><a href="/catalog/category/?id=<?php echo $item['id'];?>"><?php
-                    echo $item['name'];?></a></li>
-        <?php
+    if (isset($menuItems)) :
+        foreach ($menuItems as $item):
+            if (isset($item['link'])): ?>
+                <li><a href="<?php echo $item['link']; ?>"><?php echo $item['name']; ?></a></li>
+            <?php
+            endif;
         endforeach;
-    } ?>
+    endif;
+    ?>
 </ul>
